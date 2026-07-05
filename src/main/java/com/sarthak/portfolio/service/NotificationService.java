6c7b846cec95895e20f0e0740e2216a2b8e71641
Sarthakgite06 <sarthakgite006@gmail.com>
2026-07-05 17:34:17 +0530

    fix: correct JavaMailSender import package path

--- src/main/java/com/sarthak/portfolio/service/NotificationService.java
+++ src/main/java/com/sarthak/portfolio/service/NotificationService.java
@@ -4,7 +4,7 @@ import com.sarthak.portfolio.model.ContactMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.scheduling.annotation.Async;
 
